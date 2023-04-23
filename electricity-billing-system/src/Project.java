769e2daca116eadeacd974e55f60b39354a8c852
Abhinav Aditya <abhinavaditya162@gmail.com>
2023-04-23 16:39:05 +0530

    Bug fixes

--- electricity-billing-system/src/Project.java
+++ electricity-billing-system/src/Project.java
@@ -5,32 +5,32 @@ import java.awt.event.*;
 public class Project extends JFrame implements Custom, ActionListener {
     JFrame home;
     String atype, meter;
-
+    
     Project(String atype, String meter) {
         home = new JFrame("Electricity Billing System");
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         // home.setSize(screenSize.width, screenSize.height);
-
+        
         // Create the background image
         ImageIcon i1 = new ImageIcon(getClass().getResource("assets/bg.jpg"));
         Image i2 = i1.getImage().getScaledInstance(screenSize.width, screenSize.height, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel image = new JLabel(i3);
         home.add(image);
-
+        
         // Create a panel to hold the masterMenu bar at center of frame
         JPanel menuPanel = new JPanel();
         menuPanel.setBounds(0, 450, (screenSize.width), (screenSize.height / 10));
         menuPanel.setBackground(Color.WHITE);
         menuPanel.setOpaque(false);
         image.add(menuPanel);
-
+        
         // Create a menu bar
         JMenuBar menuBar = new JMenuBar();
         menuBar.setOpaque(false);
         menuPanel.add(menuBar);
         menuBar.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
-
+        
         // Create a Master menu
         JMenu masterMenu = new JMenu("MASTER");
         masterMenu.setForeground(Color.WHITE);
@@ -48,7 +48,7 @@ public class Project extends JFrame implements Custom, ActionListener {
         newcustomer.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
         newcustomer.setFont(new Font("Tahoma", Font.BOLD, 13));
         masterMenu.add(newcustomer);
-
+        
         JMenuItem customerdetails = new JMenuItem("Customer Details");
         customerdetails.addActionListener(this);
         customerdetails.setBackground(Color.BLACK);
@@ -56,13 +56,13 @@ public class Project extends JFrame implements Custom, ActionListener {
         customerdetails.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
         customerdetails.setFont(new Font("Tahoma", Font.BOLD, 13));
         masterMenu.add(customerdetails);
-
+        
         JMenuItem depositdetails = new JMenuItem("Deposit Details");
         depositdetails.addActionListener(this);
         depositdetails.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
         depositdetails.setFont(new Font("Tahoma", Font.BOLD, 13));
         masterMenu.add(depositdetails);
-
+        
         JMenuItem calculatebil = new JMenuItem("Calculate Bill");
         calculatebil.addActionListener(this);
         calculatebil.setBackground(Color.BLACK);
@@ -70,7 +70,7 @@ public class Project extends JFrame implements Custom, ActionListener {
         calculatebil.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
         calculatebil.setFont(new Font("Tahoma", Font.BOLD, 13));
         masterMenu.add(calculatebil);
-
+        
         // Create a Info menu
         JMenu infoMenu = new JMenu("INFO");
         infoMenu.setForeground(Color.WHITE);
@@ -88,7 +88,7 @@ public class Project extends JFrame implements Custom, ActionListener {
         updateinformation.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
         updateinformation.setFont(new Font("Tahoma", Font.BOLD, 13));
         infoMenu.add(updateinformation);
-
+        
         JMenuItem viewinformation = new JMenuItem("View Information");
         viewinformation.addActionListener(this);
         viewinformation.setBackground(Color.BLACK);
@@ -96,7 +96,7 @@ public class Project extends JFrame implements Custom, ActionListener {
         viewinformation.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
         viewinformation.setFont(new Font("Tahoma", Font.BOLD, 13));
         infoMenu.add(viewinformation);
-
+        
         // Create a User menu
         JMenu userMenu = new JMenu("USER");
         userMenu.setForeground(Color.WHITE);
@@ -107,13 +107,13 @@ public class Project extends JFrame implements Custom, ActionListener {
         menuBar.add(Box.createHorizontalGlue());
         menuBar.add(Box.createHorizontalStrut(50));
         // menuBar.add(userMenu);
-
+        
         JMenuItem paybill = new JMenuItem("Pay Bill");
         paybill.addActionListener(this);
         paybill.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
         paybill.setFont(new Font("Tahoma", Font.BOLD, 13));
         userMenu.add(paybill);
-
+        
         JMenuItem billdetails = new JMenuItem("Bill Details");
         billdetails.addActionListener(this);
         billdetails.setBackground(Color.BLACK);
@@ -121,7 +121,7 @@ public class Project extends JFrame implements Custom, ActionListener {
         billdetails.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
         billdetails.setFont(new Font("Tahoma", Font.BOLD, 13));
         userMenu.add(billdetails);
-
+        
         // Create a Report menu
         JMenu reportMenu = new JMenu("REPORT");
         reportMenu.setForeground(Color.WHITE);
@@ -132,13 +132,13 @@ public class Project extends JFrame implements Custom, ActionListener {
         menuBar.add(Box.createHorizontalGlue());
         menuBar.add(Box.createHorizontalStrut(50));
         // menuBar.add(reportMenu);
-
+        
         JMenuItem generatebill = new JMenuItem("Generate Bill");
         generatebill.addActionListener(this);
         generatebill.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
         generatebill.setFont(new Font("Tahoma", Font.BOLD, 13));
         reportMenu.add(generatebill);
-
+        
         if (atype.equals("Admin")) {
             menuBar.add(masterMenu);
         } else {
@@ -146,33 +146,33 @@ public class Project extends JFrame implements Custom, ActionListener {
             menuBar.add(userMenu);
             menuBar.add(reportMenu);
         }
-
+        
         home.setVisible(true);
         home.setExtendedState(JFrame.MAXIMIZED_BOTH);
         home.setMinimumSize(screenSize.getSize());
         home.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-
+    
     public void actionPerformed(ActionEvent ae) {
         String msg = ae.getActionCommand();
         if (msg.equals("New Customer")) {
             new NewCustomer();
         } else if (msg.equals("Customer Details")) {
-            // new CustomerDetails();
+            new CustomerDetails();
         } else if (msg.equals("Deposit Details")) {
-            // new DepositDetails();
+            new DepositDetails();
         } else if (msg.equals("Calculate Bill")) {
             new CalculateBill();
         } else if (msg.equals("View Information")) {
-            // new ViewInformation(meter);
+            new ViewInformation(meter);
         } else if (msg.equals("Update Information")) {
             new UpdateInformation(meter);
         } else if (msg.equals("Bill Details")) {
-            // new BillDetails(meter);
+            new BillDetails(meter);
         } else if (msg.equals("Pay Bill")) {
             new PayBill(meter);
         } else if (msg.equals("Generate Bill")) {
-            // new GenerateBill();
+            new GenerateBill(meter);
         }
     }
 
--- electricity-billing-system/src/Signup.java
+++ electricity-billing-system/src/Signup.java
@@ -2,20 +2,22 @@ import javax.swing.*;
 import javax.swing.border.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.sql.*;
+
 
 public class Signup extends JFrame implements Custom,ActionListener {
-
+    
     JButton btncreate,btnback;
     Choice accountType;
     JTextField meter,username,name, password;
-
+    
     Signup()
     {
         setBounds(600,300,640,400);
         getContentPane().setBackground(Color.WHITE);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
-
+        
         JPanel panel = new JPanel();
         panel.setBounds(20,30,590,300);
         panel.setBackground(secondaryColor);
@@ -23,14 +25,14 @@ public class Signup extends JFrame implements Custom,ActionListener {
         panel.setLayout(null);
         panel.setForeground(FontColor);
         add(panel);
-
+        
         // Create Labels for Account type,meter number,username,name and password
         JLabel heading = new JLabel("Create Account as");
         JLabel lblmeter = new JLabel("Meter Number");
         JLabel lblusername = new JLabel("Username");
         JLabel lblname = new JLabel("Name");
         JLabel lblpassword = new JLabel("Password");
-
+        
         // Create Text Fields for meter number,username,name and password
         accountType = new Choice();
         accountType.add("Admin");
@@ -40,7 +42,7 @@ public class Signup extends JFrame implements Custom,ActionListener {
         username = new JTextField();
         name = new JTextField();
         password = new JPasswordField();
-
+        
         // Create Buttons for create account and cancel
         btncreate = new JButton("Create");
         btnback = new JButton("Back");
@@ -51,18 +53,18 @@ public class Signup extends JFrame implements Custom,ActionListener {
         panel.add(lblusername);
         panel.add(lblname);
         panel.add(lblpassword);
-
+        
         // Add Text Fields to the frame
         panel.add(accountType);
         panel.add(meter);
         panel.add(username);
         panel.add(name);
         panel.add(password);
-
+        
         // Add Buttons to the frame
         panel.add(btncreate);
         panel.add(btnback);
-
+        
         // Set Bounds for Labels
         heading.setBounds(120,20,120,30);
         lblmeter.setBounds(120,50,120,30);
@@ -76,15 +78,15 @@ public class Signup extends JFrame implements Custom,ActionListener {
         username.setBounds(260,80,200,25);
         name.setBounds(260,110,200,25);
         password.setBounds(260,140,200,25);
-
+        
         // Set Bounds for Buttons
         btncreate.setBounds(180,200,100,30);
         btnback.setBounds(310,200,100,30);
-
+        
         // Add Action Listener to the buttons
         btncreate.addActionListener(this);
         btnback.addActionListener(this);
-
+        
         // Adding border to the text fields
         meter.setBorder(new LineBorder(Color.BLACK,1));
         username.setBorder(new LineBorder(Color.BLACK,1));
@@ -105,18 +107,51 @@ public class Signup extends JFrame implements Custom,ActionListener {
         lblname.setFont(new Font("Tahoma",Font.PLAIN,14));
         lblpassword.setFont(new Font("Tahoma",Font.PLAIN,14));
 
+
+        meter.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent fe) {}
+            
+            @Override
+            public void focusLost(FocusEvent fe) {
+                try {
+                    Conn c  = new Conn();
+                    ResultSet rs = c.s.executeQuery("select * from login where meter_no = '"+meter.getText()+"'");
+                    while(rs.next()) {
+                        name.setText(rs.getString("name"));
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        accountType.addItemListener(new ItemListener() { // 
+            public void itemStateChanged(ItemEvent ae) {
+                String user = accountType.getSelectedItem();
+                if (user.equals("Customer")) {
+                    lblmeter.setVisible(true);
+                    meter.setVisible(true);
+                    name.setEditable(false);
+                } else {
+                    lblmeter.setVisible(false);
+                    meter.setVisible(false);
+                    name.setEditable(true);
+                }
+            }
+        });
+        
         // Set color for buttons
         btncreate.setBackground(btnColor);
         btncreate.setForeground(Color.WHITE);
         btncreate.setBorderPainted(false);
-
+        
         btnback.setBackground(btnColor);
         btnback.setForeground(Color.WHITE);
         btnback.setBorderPainted(false);
-
+        
         setVisible(true);
     }
-
+    
     public void actionPerformed(ActionEvent ae)
     {
         if(ae.getSource() == btncreate)
@@ -127,7 +162,7 @@ public class Signup extends JFrame implements Custom,ActionListener {
             String sname = name.getText();
             String smeter = meter.getText();
             String spassword = password.getText();
-
+            
             try {
                 Conn c = new Conn();
                 
@@ -155,7 +190,7 @@ public class Signup extends JFrame implements Custom,ActionListener {
             this.setVisible(false);
         }
     }
-
+    
     public static void main(String[] arg)
     {
         new Signup();
